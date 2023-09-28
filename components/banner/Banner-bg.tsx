750d2d007ade8b189a49b52ef959458a27db308c
FADHILI-Josue <fadhilijosue27@gmail.com>
2023-09-28 19:55:58 +0200

    adding some images;

--- components/banner/Banner-bg.tsx
+++ components/banner/Banner-bg.tsx
@@ -11,10 +11,10 @@ interface BannerbgProps {
 const Bannerbg: FC<BannerbgProps> = ({ }) => {
     const isLargeScreen = useIsLargeScreen()
     return  <>
-        {isLargeScreen ?
-            <Image src={BannerBg} alt={'ouraring fixed home banner image'} className='absolute top-0 -z-10 object-cover' /> :
-            <Image src={MobileBannerBg} alt={'ouraring fixed home banner image'} className='-top-30 absolute -z-10 object-cover' />
-        }
+        {/* {isLargeScreen ? */}
+            <Image src={BannerBg} alt={'ouraring fixed home banner image'} className='absolute top-0 w-full h-full -z-10 object-cover' /> :
+            {/* <Image src={MobileBannerBg} alt={'ouraring fixed home banner image'} className='-top-30 absolute -z-10 object-cover' /> */}
+        {/* } */}
     </>
 
 
